8f54fb0490a7d2923db578015010bf39c7883af8
Tope Bello <Belltop12@gmail.com>
2025-01-27 11:23:39 +0100

    services added

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no:"",
+        s_name:"Web Application Dev",
+        s_desc:"Building dynamic, scalable, and user-friendly web applications tailored to client needs."
     },
     {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no:"",
+        s_name:"Database Design and Management",
+        s_desc:"Design, implement, and optimize databases (SQL or NoSQL) for efficient data storage and retrieval."
     },
     {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_no:"",
+        s_name:" Responsive and Mobile-Friendly Design",
+        s_desc:"Develop web applications that are fully responsive and optimized for all screen sizes."
     },
     {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no:"",
+        s_name:" API Development and Integration",
+        s_desc:"Create robust RESTful or GraphQL APIs to enable seamless communication between systems."
     },
     {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no:"",
+        s_name:"Maintenance, Updates, and Debugging",
+        s_desc:"Offer ongoing support to fix bugs, implement updates, and ensure optimal application performance."
     },
     {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no:"",
+        s_name:"Deployment and Hosting Solutions",
+        s_desc:"Deploy web applications on cloud platforms like AWS, Azure, Google Cloud, or Heroku."
     },
 ]
  
