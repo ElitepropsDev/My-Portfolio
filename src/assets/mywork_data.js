f390ace53d57132c126fe8c37b5ea15b4253f860
Tope Bello <Belltop12@gmail.com>
2025-02-10 15:25:37 +0100

    My work edited

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -8,37 +8,37 @@ import project6_img from '../assets/project_6.png'
 const mywork_data = [
     {
         w_no:1,
-        w_name:"food ordering/selling Ecoomerce",
+        w_name:"MERN STACK",
         w_img:project1_img,
         w_link: "https://trao-kithen-frontend.onrender.com/"
     },
     {
         w_no:2,
-        w_name:"Web design",
+        w_name:"REACT.JS/TMDB/FIREBASE",
         w_img:project2_img,
         w_link: "https://netflix-opal-ten.vercel.app/"
     },
     {
         w_no:3,
-        w_name:"Web design",
+        w_name:"REACT.JS/FIREBASE",
         w_img:project3_img,
         w_link: "https://netflix-opal-ten.vercel.app/"
     },
     {
         w_no:4,
-        w_name:"Web design",
+        w_name:"MERN STACK",
         w_img:project4_img,
         w_link: "https://trao-kithen-frontend.onrender.com/"
     },
     {
         w_no:5,
-        w_name:"Web design",
+        w_name:"REACT.JS/YOUTUBE API",
         w_img:project5_img,
         w_link: "https://youtube-blush-eight.vercel.app/"
     },
     {
         w_no:6,
-        w_name:"Web design",
+        w_name:"WORDPRESS/WOOCOMERCE",
         w_img:project6_img,
         w_link: "https://www.kaymax.ng/"
     },
