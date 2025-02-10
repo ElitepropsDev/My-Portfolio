d54fc1d7d8ecaaa8b844b7d940d921de58ca8a5b
Tope Bello <Belltop12@gmail.com>
2025-02-10 15:27:16 +0100

    my work added

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './MyWork.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import mywork_data from '../../assets/mywork_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import React from 'react';
+import './MyWork.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import mywork_data from '../../assets/mywork_data';
+import arrow_icon from '../../assets/arrow_icon.svg';
 
 const MyWork = () => {
   return (
@@ -12,16 +12,26 @@ const MyWork = () => {
             <img src={theme_pattern} alt="" />
         </div>
         <div className="mywork-container">
-            {mywork_data.map((work,index)=>{
-                return <img key={index} src={work.w_img} alt="" />
-            })}
+            {mywork_data.map((work, index) => (
+                <a 
+                    key={index} 
+                    href={work.w_link} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="work-item"
+                >
+                    <img src={work.w_img} alt={work.w_name} />
+                    <h2>{work.w_name}</h2>
+                </a>
+                
+            ))}
         </div>
         {/* <div className="mywork-showmore">
             <p>Show More</p>
             <img src={arrow_icon} alt="" />
         </div> */}
     </div>
-  )
+  );
 }
 
-export default MyWork
+export default MyWork;
