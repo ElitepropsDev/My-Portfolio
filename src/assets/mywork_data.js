ae2d41f2485ac01fe5beec0f5534f6614dade736
Tope Bello <Belltop12@gmail.com>
2025-06-06 13:12:05 +0100

    7 portfolio added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,11 +4,13 @@ import project3_img from '../assets/project_3.png'
 import project4_img from '../assets/project_4.png'
 import project5_img from '../assets/project_5.png'
 import project6_img from '../assets/project_6.png'
+import project7_img from '../assets/project_7.png'
+import project8_img from '../assets/project_8.png'
 
 const mywork_data = [
     {
         w_no:1,
-        w_name:"MERN STACK",
+        w_name:"E-COMMERCE/MERN STACK",
         w_img:project1_img,
         w_link: "https://trao-kithen-frontend.onrender.com/"
     },
@@ -26,7 +28,7 @@ const mywork_data = [
     },
     {
         w_no:4,
-        w_name:"MERN STACK",
+        w_name:"E-COMMERCE/MERN STACK",
         w_img:project4_img,
         w_link: "https://trao-kithen-frontend.onrender.com/"
     },
@@ -42,6 +44,19 @@ const mywork_data = [
         w_img:project6_img,
         w_link: "https://www.kaymax.ng/"
     },
+    {
+        w_no:7,
+        w_name:"E-COMMERCE/MERN STACK",
+        w_img:project7_img,
+        w_link: "https://www.kaymax.ng/"
+    },
+    {
+        w_no:8,
+        w_name:"WOOCOMERCE/WCMV",
+        w_img:project8_img,
+        w_link: "https://www.kaymax.ng/"
+    },
+    
 ]
  
 export default mywork_data;
